7d0bbb39d4f48d11c9bb03a59ec91a7fa3d6d2d2
Lucas Gabriel <lucasgabrielsecundino@gmail.com>
2024-06-03 12:40:21 -0300

    atualizacao

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,14 +1,16 @@
 import localImage from "../../../img/Logo.png";
 import Image from "next/image";
+import "./navbar.css"
 
 export default function NavBar() {
 
     return (
-      <div style={{ height: 75, backgroundColor: '#031017' }}>
+      <div style={{ height: 75, backgroundColor: '#031017', display: 'flex'}}>
         <Image
-            src={localImage}
-            alt="Picture of the author"
-            />
+          src={localImage}
+          alt="Picture of the author"
+        />
+        <h1>ONLIPINION</h1>
       </div>
     );
   }
