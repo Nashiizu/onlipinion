216ebdb64390abc9c66c59d2ae7e77f86aded1c3
Lucas Gabriel <lucasgabrielsecundino@gmail.com>
2024-06-10 12:55:35 -0300

    Atualização

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -4,14 +4,33 @@ import "./navbar.css"
 
 export default function NavBar() {
 
-    return (
-      <div style={{ height: 75, backgroundColor: '#031017', display: 'flex'}}>
-        <Image
-          src={localImage}
-          alt="Picture of the author"
-        />
+  return (
+    <div style={{ height: 75, backgroundColor: '#031017', display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+      <Image
+        src={localImage}
+        alt="Picture of the author"
+      />
+
+      <div className="titulo">
         <h1>ONLIPINION</h1>
       </div>
-    );
-  }
-  
+
+      <div className="nav">
+          <ul>
+            <li>
+              CATEGORIAS
+            </li>
+            <li>
+              RECOMENDAÇÕES
+            </li>
+            <li>
+              SOBRE
+            </li>
+            <li>
+              CONTATO
+            </li>
+          </ul>
+      </div>
+    </div>
+  );
+}
