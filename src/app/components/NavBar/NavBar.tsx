c5cb4c8a450b306ecdbb68f67b3e83b3921ea6b8
Nashiizu <lucasgabrielsecundino@gmail.com>
2024-06-16 02:09:52 -0300

    Atualização

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -2,35 +2,18 @@ import localImage from "../../../img/Logo.png";
 import Image from "next/image";
 import "./navbar.css"
 
-export default function NavBar() {
-
-  return (
-    <div style={{ height: 75, backgroundColor: '#031017', display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-      <Image
-        src={localImage}
-        alt="Picture of the author"
-      />
-
-      <div className="titulo">
-        <h1>ONLIPINION</h1>
-      </div>
-
-      <div className="nav">
-          <ul>
-            <li>
-              CATEGORIAS
-            </li>
-            <li>
-              RECOMENDAÇÕES
-            </li>
-            <li>
-              SOBRE
-            </li>
-            <li>
-              CONTATO
-            </li>
-          </ul>
-      </div>
-    </div>
+const Navbar = () => {
+    return (
+    <nav className="navbar">
+      <div className="navbar-logo"> <img src="/Logo.png" alt="Logo"/> </div>
+      <div className="navbar-onlipinion">ONLIPINION</div>
+      <div className="navbar-categorias">CATEGORIAS</div>
+      <div className="navbar-recomendacoes">RECOMENDAÇÕES</div>
+      <div className="navbar-sobre">SOBRE</div>
+      <div className="navbar-contato">CONTATO</div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
+
